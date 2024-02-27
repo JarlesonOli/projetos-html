46a5813fd6cffa12dc04d2963808bffbda8130e1
JarlesonOli <jarlesononly@gmail.com>
2024-02-27 02:52:19 -0400

    projetos html rodando

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('leftButton').addEventListener('click', function() {
+  document.getElementById('leftMenu').classList.toggle('hidden');
+});
+
+document.getElementById('rightButton').addEventListener('click', function() {
+  document.getElementById('rightMenu').classList.toggle('hidden');
+});
